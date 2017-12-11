e8d3e3ed1c20048c5795a5eb3de80bdd01e2b635
Carlos Gonzalez <carlosj.gf@gmail.com>
2017-12-11 19:10:41 +0100

    Refactor nkpdf
    Signed-off-by: Carlos Gonzalez <carlosj.gf@gmail.com>

---
+++ src/nkpdf_util.erl
@@ -0,0 +1,32 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 Carlos Gonzalez Florido.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+
+-module(nkpdf_util).
+-export([erlguten_grid/1]).
+
+
+erlguten_grid(PDF) ->
+    eg_pdf:save_state(PDF),
+    eg_pdf:set_fill_gray(PDF,0.75),
+    eg_pdf:set_stroke_gray(PDF,0.75),
+    eg_pdf:show_grid(PDF, a4),
+    eg_pdf:restore_state(PDF).
+
